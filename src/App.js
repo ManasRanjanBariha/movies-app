45fce2c19b0209b97151670aa89fd763bfbbb975
ManasRanjanBariha <manasbariha779@gmail.com>
2023-08-31 20:36:43 +0530

    finishing
    completing the app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import "./App.css";
+import searchImage from "./search.svg";
+import { useEffect, useState } from "react";
+import MovieCard from "./MovieCard";
+
+// 90561078
+const API_URL = "https://www.omdbapi.com?apikey=90561078";
 
 function App() {
+  const [movies, setMovies] = useState([]);
+  const [searchTerm,setSearchTerm] = useState('')
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+    console.log(data.Search);
+    setMovies(data.Search);
+    console.log("movies", movies);
+  };
+  // const [count, setCount] = useState(0)
+  useEffect(() => {
+    searchMovies("hulk");
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>MovieLand</h1>
+      <div className="search">
+        <input
+          type="text"
+          placeholder="Search for movies"
+          value={searchTerm}
+          onChange={(e) => { setSearchTerm(e.target.value)}}
+        />
+        <img src={searchImage} alt="Search" onClick={() => {searchMovies(searchTerm) }} />
+      </div>
+      {movies.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>No movies found</h2>
+        </div>
+      )}
     </div>
   );
 }
